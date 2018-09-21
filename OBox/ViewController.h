fe6d189b185ff10f99e6f0ce5f192527f164c9d8
kegebai <kegebai@gmail.com>
2018-09-21 14:04:41 +0800

    Initial Commit

---
+++ OBox/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  OBox
+//
+//  Created by kegebai on 2018/9/21.
+//  Copyright © 2018年 kegebai. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ OBoxTests/OBoxTests.m
@@ -0,0 +1,37 @@
+//
+//  OBoxTests.m
+//  OBoxTests
+//
+//  Created by kegebai on 2018/9/21.
+//  Copyright © 2018年 kegebai. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface OBoxTests : XCTestCase
+
+@end
+
+@implementation OBoxTests
+
+- (void)setUp {
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown {
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+}
+
+- (void)testExample {
+    // This is an example of a functional test case.
+    // Use XCTAssert and related functions to verify your tests produce the correct results.
+}
+
+- (void)testPerformanceExample {
+    // This is an example of a performance test case.
+    [self measureBlock:^{
+        // Put the code you want to measure the time of here.
+    }];
+}
+
+@end
